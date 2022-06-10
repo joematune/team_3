45ef9a67679d8ffcb460ca5650505d50d795f09d
Jonathan Heugh <jontas@Jonathans-MacBook-Air.local>
2022-06-10 23:45:30 +0200

    added dependencies

--- packages/next-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ packages/next-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,22 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "nextapp87c7f9c9": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "api": {
-        "nextapp": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    },
     "function": {
         "postConfirmation": {
             "Name": "string",
