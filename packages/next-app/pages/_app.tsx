9643cf5e983161c63b88aed1fbc0c67d2c5b71b4
joematune <44626877+joematune@users.noreply.github.com>
2022-06-10 16:18:01 +0200

    feat(root): add chakra
    Signed-off-by: joematune <44626877+joematune@users.noreply.github.com>

--- packages/next-app/pages/_app.tsx
+++ packages/next-app/pages/_app.tsx
@@ -1,18 +1,22 @@
-import { Web3ReactProvider } from "@web3-react/core"
-import Amplify from "aws-amplify"
-import type { AppProps } from "next/app"
-import getLibrary from "../getLibrary"
-import "../styles/globals.css"
-import awsconfig from "./../src/aws-exports"
+import { ChakraProvider } from "@chakra-ui/react";
+import { Web3ReactProvider } from "@web3-react/core";
+import Amplify from "aws-amplify";
+import type { AppProps } from "next/app";
+import getLibrary from "../getLibrary";
+import "../styles/globals.css";
+import theme from "../styles/theme";
+import awsconfig from "./../src/aws-exports";
 
-Amplify.configure(awsconfig)
+Amplify.configure(awsconfig);
 
 function NextWeb3App({ Component, pageProps }: AppProps) {
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Component {...pageProps} />
-    </Web3ReactProvider>
-  )
+    <ChakraProvider resetCSS theme={theme}>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Component {...pageProps} />
+      </Web3ReactProvider>
+    </ChakraProvider>
+  );
 }
 
-export default NextWeb3App
+export default NextWeb3App;
