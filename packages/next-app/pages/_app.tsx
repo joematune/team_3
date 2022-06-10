baf5edbaa24faa318c10afad03411a9c9a826efa
danielmaartens <danielrmaartens@gmail.com>
2022-06-10 15:43:00 +0200

    feat: added chakra-ui

--- packages/next-app/pages/_app.tsx
+++ packages/next-app/pages/_app.tsx
@@ -1,9 +1,12 @@
 import { Web3ReactProvider } from "@web3-react/core"
 import Amplify from "aws-amplify"
-import type { AppProps } from "next/app"
+import { AppProps } from "next/app"
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
+
 import getLibrary from "../getLibrary"
 import "../styles/globals.css"
-import awsconfig from "./../src/aws-exports"
+import awsconfig from "../src/aws-exports"
+import { theme } from "../src/theme"
 
 Amplify.configure(awsconfig)
 
